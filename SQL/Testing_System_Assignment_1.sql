9a5cf52fabac828a776f2612c865130e15caae8a
hungndcc <quochung1110@gmail.com>
2020-12-07 02:03:54 +0700

    import data

--- SQL/Testing_System_Assignment_1.sql
+++ SQL/Testing_System_Assignment_1.sql
@@ -2,84 +2,90 @@ DROP DATABASE if exists TestingSystem;
 CREATE DATABASE TestingSystem;
 USE TestingSystem;
 
-INSERT INTO `User` ('Username',    'Email',      'Password',       'FullName',      '')
 
 CREATE TABLE Department (
-	DepartmentID 		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    DepartmentName 		VARCHAR(50) NOT NULL
+	DepartmentID 		TINYINT 		AUTO_INCREMENT,
+    DepartmentName 		VARCHAR(50) 	NOT NULL,
+    PRIMARY KEY (DepartmentID)
 );  
 
 CREATE TABLE `Position`(
-	PositionID 			INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    PositionName 		VARCHAR(55) NOT NULL
+	PositionID 			INT UNSIGNED 	AUTO_INCREMENT,
+    PositionName 		VARCHAR(55) 	NOT NULL,
+    PRIMARY KEY (PositionID)
 );
 
 CREATE TABLE `Account`(
-	AccountID 			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Email 				VARCHAR(255) UNIQUE,
-    Username 			VARCHAR(50) UNIQUE NOT NULL CHECK (length(Username) > 6),
-    Fullname 			VARCHAR(50) NOT NULL,
-    DepartmentID 		TINYINT UNSIGNED,
-    PositionID 			TINYINT UNSIGNED DEFAULT(1),
+	AccountID 			TINYINT 		NOT NULL,
+    Email 				VARCHAR(255) 	NOT NULL		UNIQUE,
+    Username 			VARCHAR(50) 	NOT NULL		UNIQUE		CHECK (length(Username) > 6),
+    Fullname 			VARCHAR(50) 	NOT NULL,
+    DepartmentID 		TINYINT,
+    PositionID 			TINYINT 		DEFAULT(1),
     CreateDate 			DATE,
-						FOREIGN KEY (DepartmentID) REFERENCES Department(DepartmentID)
+    FOREIGN KEY (DepartmentID) 			REFERENCES Department(DepartmentID),
+    PRIMARY KEY (AccountID)
 );
 
 CREATE TABLE `Group`(
-	GroupID 			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	GroupName 			VARCHAR(50),
-	CreatorID 			TINYINT UNSIGNED,
+	GroupID 			TINYINT 		AUTO_INCREMENT,
+	GroupName 			VARCHAR(50)		NOT NULL,
+	CreatorID 			TINYINT 		NOT NULL,
 	CreateDate 			DATE,
-						FOREIGN KEY (CreatorID) REFERENCES `Account`(AccountID)
+    FOREIGN KEY (CreatorID) 			REFERENCES `Account`(AccountID),
+    PRIMARY KEY (GroupID)
 );
 
 CREATE TABLE GroupAccount(
-	GroupID 			TINYINT UNSIGNED,
-    AccountID 			TINYINT UNSIGNED,
+	GroupID 			TINYINT 		NOT NULL,
+    AccountID 			TINYINT 		NOT NULL,
     JoinDate 			DATE,
-						FOREIGN KEY (GroupID) 	REFERENCES `Group`(GroupID),
-						FOREIGN KEY (AccountID) REFERENCES `Account`(AccountID)
+	FOREIGN KEY (GroupID) 				REFERENCES `Group`(GroupID),
+	FOREIGN KEY (AccountID) 			REFERENCES `Account`(AccountID)
 );
 
 CREATE TABLE TypeQuestion(
-	TypeID 				TINYINT PRIMARY KEY AUTO_INCREMENT,
-    TypeName 			VARCHAR(50) 
+	TypeID 				TINYINT  		NOT NULL,
+    TypeName 			VARCHAR(50),
+    PRIMARY KEY (TypeID)
 );
 
 CREATE TABLE CategoryQuestion(
-	CategoryID 			TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    CategoryName 		ENUM('Java','.NET','SQL','Postman','Ruby')
-);
+	CategoryID 			TINYINT 		NOT NULL 		AUTO_INCREMENT,
+    CategoryName 		ENUM('Java','.NET','SQL','Postman','Ruby'),
+    PRIMARY KEY (CategoryID) 
+) ;
 CREATE TABLE Question(
-	QuestionID 			TINYINT PRIMARY KEY AUTO_INCREMENT,
+	QuestionID 			TINYINT 		NOT NULL 		AUTO_INCREMENT,
     Content 			VARCHAR(255),
-    CategoryID 			TINYINT UNSIGNED,
-    TypeID 				TINYINT,
-    CreatorID 			TINYINT UNSIGNED,
+    CategoryID 			TINYINT 		NOT NULL,
+    TypeID 				TINYINT			NOT NULL,
+    CreatorID 			TINYINT 		NOT NULL,
     CreatrDate 			DATE,
-						FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
-						FOREIGN KEY (TypeID) 		REFERENCES TypeQuestion(TypeID),
-						FOREIGN KEY (CreatorID) 	REFERENCES `Account`(AccountID)
+	FOREIGN KEY (CategoryID) 			REFERENCES CategoryQuestion(CategoryID),
+	FOREIGN KEY (TypeID) 				REFERENCES TypeQuestion(TypeID),
+	FOREIGN KEY (CreatorID) 			REFERENCES `Account`(AccountID),
+    PRIMARY KEY (QuestionID)
 );
 
 CREATE TABLE Exam(
-	ExamID 				TINYINT PRIMARY KEY,
+	ExamID 				TINYINT ,
     `Code`				VARCHAR(50),
     Title 				VARCHAR(50),
-    CategoryID 			TINYINT UNSIGNED,
+    CategoryID 			TINYINT 		NOT NULL,
     Duration 			TIME,
-    CreatorID 			TINYINT UNSIGNED,
+    CreatorID 			TINYINT 		NOT NULL,
     CreateDate 			DATE,
-						FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
-						FOREIGN KEY (CreatorID) 	REFERENCES `Account`(AccountID)
+	FOREIGN KEY (CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
+	FOREIGN KEY (CreatorID) 	REFERENCES `Account`(AccountID),
+	PRIMARY KEY (ExamID)
 );
 
 CREATE TABLE ExamQuestion(
 	ExamID 				TINYINT,
     QuestionID 			TINYINT,
-						FOREIGN KEY (ExamID) 		REFERENCES Exam(ExamID),
-						FOREIGN KEY (QuestionID) 	REFERENCES Question(QuestionID)
+	FOREIGN KEY (ExamID) 		REFERENCES Exam(ExamID),
+	FOREIGN KEY (QuestionID) 	REFERENCES Question(QuestionID)
 );
-account
 
 
---
+++ SQL/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Account` (`AccountID`,   `Username`,      `Email`,                    `FullName`,        `Age`,  `Address`  )  VALUES        
+(1,      'hungndcc',   'hungndcc@gmail.com',      'Nguyễn Quốc Hùng',  24,   'Nam Định'  ),      
+(2,      'phucndcc',   'minhphuc18@gmail.com',      'Đoàn Minh Phúc'  ,  25,   'Nam Định'  ),                         
+(3,      'phuongk',   'vuongducphuong@gmail.com',     'Vương Đức Phương',  21,   'Hải Dương'  ),                         
+(4,      'ninhdelta',  'ninh.delta@gmail.com',      'Lê Quang Ninh'   ,  24,   'Nam Định'  ),                         
+(5,      'nvhvt1992',  'nvhvt1992@gmail.com',      'Nguyễn Vĩnh Hà',  28,   'Việt Trì'  ),                         
+(6,      'chuong9',   'chuongcao99@gmail.com',     'Cao Thế Chương',  24,   'Nam Định'  ),                         
+(7,      'kaidinh97',  'vudk97@gmail.com',       'Vũ Đình Khải',   24,   'Nam Định'  ),                         
+(8,      'tintin',   'tintintin@gmail.com',      'Đặng Đức Tin',   24,   'Bắc Ninh'  ),                         
+(9,      'phuong2k',   'phuong2k@gmail.com',      'Nguyễn Thu Phương', 21,   'Việt Trì'  ),                         
+(10,     'caixedap',   'hungndcc@gmail.com',      'Nguyễn Quốc Hùng',  24,   'Nam Định'  )
